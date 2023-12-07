b2e64e6b2867aa2e12d9c5bb896b9121c3b88ae8
Trun6123 <130743352+Trun6123@users.noreply.github.com>
2023-12-07 19:58:55 +0700

    Test Role Add

---
+++ Areas/Admin/Pages/Role/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.Admin.Role
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
