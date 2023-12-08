94bd0018e22a3032f72385db3bcb7a4cb031c940
Trun6123 <130743352+Trun6123@users.noreply.github.com>
2023-12-08 15:20:39 +0700

    Cap nhat mau bang

---
+++ ASm1670final/Controllers/UserAdminController.cs
@@ -0,0 +1,31 @@
+﻿using ASm1670final.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASm1670final.Controllers
+{
+    public class UserAdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _db;
+
+        public UserAdminController(UserManager<IdentityUser> userManager, ApplicationDbContext db)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult User()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+    }
+}
